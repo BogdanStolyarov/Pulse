b1a44a1219ac58d1623ce41e80b043e7a5460277
Bogdan <bogdemona@mail.ru>
2021-05-18 12:37:59 +0300

    fix

--- src/js/script.js
+++ src/js/script.js
@@ -131,6 +131,14 @@ $(document).ready(function () {
 		}
 	});
 
+	$("a[href=#]").click(function () {
+		const _href = $(this).attr("href");
+		$("html, body").animate({
+			scrollTop: $(_href).offset().top + "px"
+		});
+		return false;
+	});
+
 
 
 });
